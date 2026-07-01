8da2cbd61fda25c76ec62b76469b4dc770c3e38b
indo <indi37866@gmail.com>
2026-07-01 10:39:00 +0300

    fix: добавил метод кешбека к основной ветке, убрал пустые фигурные скобки в классе и ненужные круглые в классах

--- src/main/06_classes_and_interfaces/Main.kt
+++ src/main/06_classes_and_interfaces/Main.kt
@@ -4,5 +4,6 @@ fun main() {
     val myCard = BankCard()
     val myWallet = CryptoWallet()
     myCard.pay(1000)
+    myCard.applyCashback(1000)
     myWallet.pay(1000)
 }
--- src/main/06_classes_and_interfaces/PaymentMethod.kt
+++ src/main/06_classes_and_interfaces/PaymentMethod.kt
@@ -5,15 +5,14 @@ abstract class PaymentMethod {
     abstract fun pay (amount: Int)
 }
 
-class BankCard (): PaymentMethod(), Cashbackable {
+class BankCard : PaymentMethod(), Cashbackable {
     override fun pay(amount: Int) {
         balance -= amount
         println("Оплата картой на сумму $amount. Остаток $balance")
     }
-    override fun applyCashback(amount: Int) {}
 }
 
-class CryptoWallet (): PaymentMethod() {
+class CryptoWallet : PaymentMethod() {
     override fun pay(amount: Int) {
         balance -= amount
         println("Оплата криптой на сумму $amount. Остаток: $balance")
