1165e3f5ebf6f784bc048d9a568a3d1dd35753f3
indo <indi37866@gmail.com>
2026-07-05 22:53:15 +0300

    fix: выставил переменную price изменяемой

--- src/main/07_constructors/User.kt
+++ src/main/07_constructors/User.kt
@@ -1,12 +1,12 @@
 import java.security.interfaces.RSAMultiPrimePrivateCrtKey
 
-class  Product (val name: String, val price: Int) {
+class  Product (val name: String, var price: Int) {
     var discount: Int = 0
     init {
         println("Товар $name успешно добавлен на склад. Базовая цена: $price")
     }
     constructor(name: String, price: Int, discountPercent: Int = 20) : this(name, price) {
-        price -= price * discountPercent / 100
+        this.price -= price * discountPercent / 100
         this.discount = discountPercent
         println("Применена скидка ${discountPercent}%. Новая цена товара: $price")
     }
