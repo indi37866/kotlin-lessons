a7167f19d48b89de9227540544c035116c630385
indo <indi37866@gmail.com>
2026-07-07 17:44:07 +0300

    fix: убрал лишнее двоеточие и исправил структуру Элвиса Пресли

--- src/main/08_null_safety/Main.kt
+++ src/main/08_null_safety/Main.kt
@@ -1,6 +1,6 @@
-fun main (): {
+fun main () {
     val name = "Alex"
     val biography: String? = null
-    biography:? println("Информация не укзаана")
-    println("Пользователь: $name . О себе: $biography")
+    val finalBiography = biography ?: "Информация не укзаана"
+    println("Пользователь: $name . О себе: ${finalBiography}")
 }
