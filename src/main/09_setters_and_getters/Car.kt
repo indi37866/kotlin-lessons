444b8a4d98fd661dbeb03bf323366c60839cf4c6
indo <indi37866@gmail.com>
2026-07-11 22:28:19 +0300

    add: задача на полиморфизмы и повторение прошлых тем

--- src/main/09_setters_and_getters/Car.kt
+++
@@ -1,8 +0,0 @@
-package setandget_09
-
-class Hero () {
-    val isUltimateReady: Boolean
-        get() = mana > 80
-    var mana: Int = 100
-        set(value: Int) {field = value.coerceIn(0, 150)}
-}
--- src/main/09_setters_and_getters/Main.kt
+++ src/main/09_setters_and_getters/Main.kt
@@ -1,6 +1,3 @@
 package  setandget_09
 fun main() {
-    val firstPlayer = Hero()
-    firstPlayer.mana -= 30
-    println(firstPlayer.isUltimateReady)
 }
