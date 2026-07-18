91d941cee9ab4e454235c93bb5d3ea62817ebb4a
indo <indi37866@gmail.com>
2026-07-18 12:09:39 +0300

    fix: исправил ошибку в подаче типа данных в функцию

--- src/main/Revision/Main.kt
+++ src/main/Revision/Main.kt
@@ -2,9 +2,10 @@ package revision
 
 fun main() {
     val allTransactions = listOf<Double>(150.5, 0.0, 12_000.0, -50.0, 25_000.0)
+    filterSuspiciousTransactions(allTransactions)
 }
 
-fun filterSuspiciousTransactions(list: listof<Double>) {
+fun filterSuspiciousTransactions(list: List<Double>) {
     val newList = list.filter {it > 10_000.0}
     println("Найдено ${newList.count()} подозрительных транзакций.")
     println(newList)
