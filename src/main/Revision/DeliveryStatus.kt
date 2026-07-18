991344c074c5b6f1d0eb355fa347e8199de67ca3
indo <indi37866@gmail.com>
2026-07-18 14:56:44 +0300

    fix: убрал лишние скобки у класса и обезопасил цикл от иных значений

--- src/main/Revision/DeliveryStatus.kt
+++ src/main/Revision/DeliveryStatus.kt
@@ -1,11 +1,11 @@
 package revision
 
-class RobotVacuum() {
+class RobotVacuum {
     var battery: Int = 80
     fun charge() {
-        while (battery != 100) {
+        while (battery < 100) {
             battery += 5
-            println("Зарядка: ${battery}")
+            println("Зарядка: $battery")
         }
         println("Робот готов к уборке")
     }
