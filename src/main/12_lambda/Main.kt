7a97cea5a67d18728052fbb62e06f25d6367587b
indo <indi37866@gmail.com>
2026-07-19 14:22:27 +0300

    add: Новая задача на лямбды

--- src/main/12_lambda/Main.kt
+++ src/main/12_lambda/Main.kt
@@ -1,13 +1,7 @@
 package lambda12
 
 fun main() {
-    val operation: (Int, Int, Operation) -> Int
-    operation = {firstNum: Int, secondNum: Int, oper: Operation -> when (oper) {
-        Operation.MINUS -> firstNum - secondNum
-        Operation.PLUS -> firstNum + secondNum
-        Operation.MULTIPLY -> firstNum * secondNum
-        }
-    }
-    val result = operation(12, 23, Operation.MULTIPLY)
-    println(result)
+    val contactChecker = fun(text: String?) {println(text?: "Текст отсутствует")}
+    println(contactChecker(null))
 }
+
