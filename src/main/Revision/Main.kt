b0ba46197eb3fd317f058cc56b7166c616fe9b7f
indo <indi37866@gmail.com>
2026-07-28 17:05:19 +0300

    Refactor: split Product class and main logic

--- src/main/Revision/Main.kt
+++ src/main/Revision/Main.kt
@@ -1,1 +1,9 @@
 package revision
+
+fun main() {
+    val product1 = Product("Яблоко", 10.5)
+    println(product1)
+    val product2 = product1.copy(price = 11.0)
+    println(product1 == product2)
+    println(product1 == product1.copy())
+}
