e53f546481fca0d7e68e797b426954dd0b94c1f5
indo <indi37866@gmail.com>
2026-08-11 12:52:46 +0300

    add: задача на extension функции и свойства

--- src/main/Revision/Main.kt
+++ src/main/Revision/Main.kt
@@ -1,9 +1,20 @@
 package revision
 
 fun main() {
-    val product1 = Product("Яблоко", 10.5)
-    println(product1)
-    val product2 = product1.copy(price = 11.0)
-    println(product1 == product2)
-    println(product1 == product1.copy())
+    val firstLoad = NetworkResult.Success(123, "Усех")
+    val secondLoad = NetworkResult.Error("Недостаточно средств")
+    val thirdLoad = NetworkResult.Loading
+    println(firstLoad.toLogMessage())
+    println(secondLoad.toLogMessage())
+    println(thirdLoad.toLogMessage())
+}
+
+
+fun NetworkResult.toLogMessage(): String {
+    return when (this) {
+        is NetworkResult.Success -> "Успех [код ${code}]: $data"
+        is NetworkResult.Error -> "Ошибка: $message"
+        is NetworkResult.Loading -> "Загрузка данных..."
+    }
+
 }
--- src/main/Revision/Product.kt
+++ src/main/Revision/Product.kt
@@ -1,4 +1,7 @@
 package revision
 
-data class Product (val name: String, val price: Double, val instock: Boolean = true) {
+sealed class NetworkResult {
+    data class Success(val code: Int, val data: String): NetworkResult()
+    data class Error(val message: String): NetworkResult()
+    data object Loading: NetworkResult()
 }
